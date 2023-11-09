c8d2ff17d38cac234071a776b60671ed6f4c682f
Mohammed Shiraz <lightningboy78@gmail.com>
2023-11-08 22:01:04 -0500

    Register, Login, Update User Profile has been added.

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -23,12 +23,13 @@ urlpatterns = [
     path("supersecret/", admin.site.urls),
     path("api/v1/auth/", include('djoser.urls')),
     path("api/v1/auth/", include('djoser.urls.jwt')),
-    path("api/v1/profile/", include("apps.profiles.urls"))
-    path("api/v1/properties/", include("apps.properties.urls"))
-    path("api/v1/ratings/", include("apps.ratings.urls"))
-    path("api/v1/enquiries/", include("apps.enquiries.urls"))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
+    path("api/v1/profile/", include("apps.profiles.urls")),
+    path("api/v1/properties/", include("apps.properties.urls")),
+    path("api/v1/ratings/", include("apps.ratings.urls")),
+    path("api/v1/enquiries/", include("apps.enquiries.urls")),
+]# + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# Commented out the above code because we are no longer serving things locally.
+# We are usig nginx for that.
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = 'Real Estate Admin Portal'
 admin.site.index_title = 'Welcome to the Real Estate Portal'
